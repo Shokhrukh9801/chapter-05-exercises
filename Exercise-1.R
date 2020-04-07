bae9f7fdb0b762a031700bd36a60023d2e9cb9e6
Shokhrukh <mr.shox1998@gmail.com>
2020-04-07 17:52:37 +0900

    Exercise-1

---
+++ Exercise-1.R
@@ -0,0 +1,14 @@
+hometown <- 'Samarkand'
+my_name <-'Shokhrukh'
+my_height <- '68.11'
+puppies <- '2'
+puppy1_price <- 1100
+puppy2_price <- 300
+total_cost <- puppy1_price + puppy2_price
+print(total_cost)
+too_expensive=total_cost
+too_expensive > 1000
+(puppy1_price > puppy2_price)
+max_puppies <- puppy1_price
+
+
